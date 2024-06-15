447be7a0fce27f80203eeccb255bec9ef678f0aa
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-15 04:00:15 +0200

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 export { get_events_by_calendar_id } from "./api/get_events_by_calendar_id";
 export { get_subscribed_by_username } from "./api/get_subscribed_by_username";
 export { preferences_by_properties } from "./api/preferences_by_properties";
+export { get_moodle_courses } from "./api/get_moodle_courses";
+export { login_check } from "./api/login_check";
+
+export { oauth2_authorize } from "./cas/oauth2/authorize";
+export { oauth2_token } from "./cas/oauth2/token";
+export { oauth2_userinfo } from "./cas/oauth2/userinfo";
