f7fae9145161e09361a10359689939be0d7e9eb1
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-15 03:58:52 +0200

    chore(consts): add more endpoints with challenge values

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,8 @@
 export const PROD_ENDPOINT = "https://apis.unilim.fr";
 export const DEV_ENDPOINT = "https://apis-dev.unilim.fr";
+export const BIOME_PROD_ENDPOINT = "https://biome.unilim.fr";
+export const BIOME_DEV_ENDPOINT = "https://biome-dev.unilim.fr";
+
+export const OAUTH2_CODE_CHALLENGE_METHOD = "plain";
+export const OAUTH2_CODE_CHALLENGE = Math.random().toString(36).substring(2);
+export const OAUTH2_STATE = Math.random().toString(36).substring(2);
