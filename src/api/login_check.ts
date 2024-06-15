c8e58662b396d8c0cc48d95583551955cb95e69c
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-15 03:59:45 +0200

    feat(api): add login_check to get API token

---
+++ src/api/login_check.ts
@@ -0,0 +1,19 @@
+import { DEV_ENDPOINT, PROD_ENDPOINT } from "~/utils/constants";
+
+/**
+ * @returns A token to use for the API (`apis.unilim.fr`), you should still
+ * use the access_token for the CAS API (`cas.unilim.fr`) and this request.
+ */
+export const login_check = async (access_token: string, useDevEndpoint = false) => {
+  const response = await fetch(`${useDevEndpoint ? DEV_ENDPOINT : PROD_ENDPOINT}/api/login_check`, {
+    method: "POST",
+    headers: { "X-Auth-Token": access_token }
+  });
+
+  const json = await response.json() as { token: string } | { code: number, message: string };
+  if ("code" in json) {
+    throw new Error(json.message);  
+  }
+
+  return json;
+};
