84e5b938ce068748399375b99f76818d1e48e320
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-15 20:50:37 +0200

    refactor(examples): rename to oauth2

--- examples/biome_oauth2.ts
+++ examples/biome_oauth2.ts
@@ -6,7 +6,7 @@ const password = "";
 void async function main () {
   const code = await oauth2_authorize({ username, password });
   const { access_token } = await oauth2_token(code);
-  const { name } = await oauth2_userinfo(access_token);
+  const { email, name, sub } = await oauth2_userinfo(access_token);
 
-  console.log("User:", name);
+  console.log("Logged to", sub, "with email", email, "and name", name);
 }();
